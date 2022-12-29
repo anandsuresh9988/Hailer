9823f2d4dafb619e74c85a34e11cb348700c9c3f
anandsuresh9988 <anandsuresh9988@gmail.com>
2022-12-29 17:28:13 +0000

    1) Removed main Makefile 2) Modified test_hailer.sh 3) Improvements to Test Apps: Fixed high CPU usage, intialised msg hdr
    Signed-off-by: anandsuresh9988 <anandsuresh9988@gmail.com>

--- test-app/test_hailer.sh
+++ test-app/test_hailer.sh
@@ -3,15 +3,11 @@
 # Simple shell script to enable easy testing of HAILER for dev purposes
 # Run with sudo privilages --> sudo ./test_hailer.sh
 
-(cd .. && make ) 
-gcc test_app_a.c -I../src/include -lhailer -lpthread -o test_app_a.exe;
-gcc test_app_b.c -I../src/include -lhailer -lpthread -o test_app_b.exe;
-
-killall test_app_a.exe;
+killall hailer_test_a;
 sleep 1;
-killall test_app_b.exe;
+killall hailer_test_b;
 sleep 1;
 killall hailer_server;
 rm -f /var/.hailer_server_address.sock;
 
-../bin/hailer_server wlan0 &
+../bin/bin/hailer_server wlan0 &
